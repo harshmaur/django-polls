1d1d191c65265cfa0d1f31d6aa825c8c0379fc9e
Harsh Maur <harshmaur@gmail.com>
2015-06-09 23:49:12 +0530

    Completed Django Polls

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = (
     'polls',
 )
 
+
+
+
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -86,3 +89,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
